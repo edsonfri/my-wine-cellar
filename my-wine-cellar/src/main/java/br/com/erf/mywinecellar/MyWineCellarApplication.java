ab5c0845fc168adaf966e99c17e31655153e33cd
Edson Friedrich <edsonfri@gmail.com>
2024-05-09 09:31:57 -0300

    Commit inicial

--- my-wine-cellar/src/main/java/br/com/erf/mywinecellar/MyWineCellarApplication.java
+++ my-wine-cellar/src/main/java/br/com/erf/mywinecellar/MyWineCellarApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyWineCellarApplication {
+	public String PORT = System.getenv( "PORT");
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyWineCellarApplication.class, args);
