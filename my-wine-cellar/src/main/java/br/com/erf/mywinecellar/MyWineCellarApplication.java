fc6e86b83093e5f092fbb1cbd118e1dd0108c696
Edson Friedrich <edsonfri@gmail.com>
2024-05-09 12:49:58 -0300

    ajuste variavel de ambiente

--- my-wine-cellar/src/main/java/br/com/erf/mywinecellar/MyWineCellarApplication.java
+++ my-wine-cellar/src/main/java/br/com/erf/mywinecellar/MyWineCellarApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyWineCellarApplication {
 	public String PORT = System.getenv( "PORT");
+	public String URI = System.getenv( "URI");
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyWineCellarApplication.class, args);
